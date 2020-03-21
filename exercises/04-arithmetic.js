2bf1c5104774c6f0d0de1cec8446bce1db0f0a9a
ray-vazquez <rayvazquez125@gmail.com>
2020-03-21 14:00:45 -0400

    first commit

---
+++ exercises/04-arithmetic.js
@@ -0,0 +1,78 @@
+///////////////////////////////////////////////
+//////////// Arithmetic Operations ////////////
+///////////////////////////////////////////////
+/** 
+* @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+  @see https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Math
+*/
+
+// using Arithmetic Operations, solve the following
+
+/**
+ * Add two numbers
+ *
+ * @param {*} a
+ * @param {*} b
+ * @returns {Number} Number
+ */
+const add = (a, b) => {};
+
+/**
+ * Subtract two numbers
+ *
+ * @param {*} a
+ * @param {*} b
+ * @returns {Number} Number
+ */
+const sub = () => {};
+/**
+ * Multiply two numbers
+ *
+ * @param {*} a
+ * @param {*} b
+ * @return {Number} Number
+ */
+const mul = () => {};
+
+/**
+ * Divide two numbers
+ * @param {*} a
+ * @param {*} b
+ * @return {Number} Number
+ */
+const divide = () => {};
+
+
+
+/**
+ * Given the following:
+ * I own 3 pet carriers and 8 cats.
+ * All the cat need vacinations.
+ * I can fit 2 cats per carrier. (they get along great)
+ * How many cats stay home on the first trip to the vet? 
+ */
+
+const remainder = () => { 
+  
+};
+
+
+
+/** 
+*  
+How much money do I have if at the start of the day I had $94.
+I spent $4.25 on coffee. A friend gave me $7 that he owed me. 
+I went out for a meal. The bill was $23.50, but I also gave a 20% tip.
+Some friends and I found $20 on the ground and split it four ways.
+
+hint: your answer should total 73.55
+*/
+
+
+/**
+ * Create four variables that contain numbers. Call the variables something sensible.
+  1. Add the first two variables together and store the result in another variable.
+  2. Subtract the fourth variable from the third and store the result in another variable.
+  3. Multiply the results from the last two steps together, storing the result in a variable called finalResult. The product should be 48. If it isn't, you'll have to adjust some of the initial input values.
+  4. Finally, write another function that checks whether finalResult is an even number. Store the result in a variable called evenOddCheck.
+ */
