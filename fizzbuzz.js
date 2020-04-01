052c57e3e5af81e6d2935df226dc26d13558f1a1
ray-vazquez <rayvazquez125@gmail.com>
2020-04-01 18:51:55 -0400

    adding fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,6 @@
+// FIZZBUZZ
+
+// for each number from 1 through 100
+// if the number is divisible by 3 console.log 'Fizz'
+// if the number is divisble by 5 console.log "Buzz"
+// if the number is divisble by both console.log 'Fizzbuzz'
